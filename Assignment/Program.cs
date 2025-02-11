e258b1ed0e7fbbd1a822948584644dd2fbf6368f
abdallh.mahfouz97@outlook.com <abdallh.mahfouz97@outlook.com>
2025-02-11 22:57:59 +0200

    #AllisDone

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -179,6 +179,71 @@ namespace Assignment
             #endregion
             #endregion
 
+            #region Transformation Operators
+            #region 1. Return a sequence of just the names of a list of products.
+            //var Result = ProductsList.Select(X => X.ProductName);
+            //foreach (var Product in Result)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+            #endregion
+            #region 2. Produce a sequence of the uppercase and lowercase versions of each word in the original array (Anonymous Types).
+            //string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+            //var Result=words.Select(X => new{LowerCase =X.ToLower(),UpperCase= X.ToUpper()});
+            //foreach (var word in Result)
+            //{
+            //    Console.WriteLine(word);
+            //}
+            #endregion
+            #region 3. Produce a sequence containing some properties of Products, including UnitPrice which is renamed to Price in the resulting type
+            //var Reslut = ProductsList.Select(X => new
+            //                            { Name =X.ProductName,
+            //                              Price=X.UnitPrice
+            //                            });
+            //foreach (var item in Reslut)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+            #region 4. Determine if the value of int in an array matches their position in the array.
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Select((X, I) => X == I).ToArray();
+            //for (int i = 0; i < Arr.Length; i++) 
+            //{
+            //    Console.WriteLine($"{Arr[i]}: {(Result[i] ? "Match" : "Not Match")}");
+            //}
+
+            #endregion
+            #region 5. Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+            //var Result= numbersA.SelectMany(a =>
+            //            numbersB.Where(b => a < b),
+            //            (a, b) => (a, b));
+            //Console.WriteLine("Pairs where a < b");
+            //foreach (var i in Result)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region 6. Select all orders where the order total is less than 500.00.
+            //var Result = CustomersList.SelectMany(x => x.Orders.Where(y => y.Total < 500.00M));
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region 7. Select all orders where the order was made in 1998 or later.
+            //var Result = CustomersList.SelectMany(x => x.Orders.Where(y => y.OrderDate >= new DateTime(1998, 1, 1)));
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #endregion
+
 
 
 
